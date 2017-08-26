022d104c9a112fbfe204f629459f4597b174c483
Noriah Williams <willnoriah23@gmail.com>
2017-08-26 09:50:36 -0400

    Homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,44 @@
-//Letter choices available
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-//Setting all to zero
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+// array for user's guesses
 var guessedLetters = [];
-var userGuess = null;
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " guessedLetters " + guessedLetters );
-
-
+var userGuess = "null";
+///Letter choices available
+var alphabetChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+console.log('key');
+    console.log('event.key: ', event.key);
 document.onkeyup = function(event) {
     // String.fromCharCode converts a number into a character from an array
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	document.getElementById("guesses-made").innerHTML +=  event.key;
+    var userGuess = event.key;
+    document.getElementById("guesses-made").innerHTML =  event.key;
 
-    //indexOf locates values in an array
-    if (guessedLetters.indexOf(userGuess) < 0 && computerChoices.indexOf(userGuess) >= 0) {
-        guessedLetters[guessedLetters.length] = userGuess;
-        guessesLeft--;
-        document.getElementById("guessedLetters").innerHTML += event.key;
-    }
-    // == checks if a value is equal to another value
-    if (computerGuess == guessedLetters) {
-        wins++;
-        console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " guessedLetters " + guessedLetters );
-        document.getElementById("losses").innerHTML += event.key
-    }
+     //indexOf locates values in an array
+    // if (guessedLetters.indexOf(userGuess) < 0 && alphabetChoices.indexOf(userGuess) >= 0) {
+    //     console.log('inside first if/else');
+    //     guessedLetters[guessedLetters.length] = userGuess;
+    //     guessesLeft--;
+    //     document.getElementById("guesses-made").innerHTML += event.key;
+    // }
 
-    if (guessesLeft == 0) {
-        losses++;
-        guessesLeft = 9;
-        guessedLetters = [];
-        computerGuess = computerChoices[Math.floor(Math.random() * (computerChoices.length))];
-        console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " guessedLetters " + guessedLetters);
-        document.getElementById("guesses-left").innerHTML += event.key
-    }
-}
+    //  // == checks if a value is equal to another value
+    // if (computerGuess == guessedLetters) {
+    //     console.log('inside second');
 
+    //     wins++;
+    //     document.getElementById("losses").innerHTML += event.key;
+    //     console.log("Wins: " + wins + " Losses: " + losses + " Guesses Made: " + guesses-made + " guessedLetters " + guessedLetters);
 
+    
+    // if (guessesLeft == 0) {
 
+    //     console.log('inside third');
 
+    //     losses++;
+    //     guessesLeft = 9;
+    //     guessedLetters = [];
+    //     computerGuess = alphabetChoices[Math.floor(Math.random() * (alphabetChoices.length))];
+    //     document.getElementById("guesses-left").innerHTML += event.key;
+    //     console.log("Wins: " + wins + " Losses: " + losses + " Guesses Made: " + guesses-made + " guessedLetters " + guessedLetters);    
+    //  }   
+};
